ff1613b292e9cd00b5b5872b6e0daa3e4a4a7de4
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2024-03-29 15:40:56 +0000

    only allow heroku host

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-vcwsnc61pnw)b%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['radiant-basin-91055-10bb7ac3964f.herokuapp.com']
 
 
 # Application definition
